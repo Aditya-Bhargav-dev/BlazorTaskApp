8244095f0124688bed009de5a2de364501a3da0d
Aditya Bhargav <adityabarghav788@gmail.com>
2025-02-08 15:15:59 -0600

    Commit changes for task 1 and task 2

---
+++ BlazorTaskApp-main/BlazorTaskApp/BlazorTaskApp/Data/BlazorTaskAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BlazorTaskApp.Models;
+
+namespace BlazorTaskApp.Data
+{
+    public class BlazorTaskAppContext : DbContext
+    {
+        public BlazorTaskAppContext (DbContextOptions<BlazorTaskAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BlazorTaskApp.Models.Book> Book { get; set; } = default!;
+    }
+}
